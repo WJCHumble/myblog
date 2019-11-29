17b0f183ae5db70289b921ea695991cc4df33b2d
WJCHumble <1957503379@qq.com>
2019-11-29 23:41:37 +0800

    fix：关于我们页面初始化

--- src/view/About/index.vue
+++ src/view/About/index.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-       About 
+    <div class="about">
+        <div class="profile">
+            <div class="title">
+                个人简介
+            </div>
+            <div class="content">
+                <div class="avatar">
+                    <img src="https://wjchumble.oss-cn-hangzhou.aliyuncs.com/myblog/avatar.jpg" alt="">
+                </div>
+                性别男，无名本科在校大四，自学编程一年，自学过Java、C语言、C#、PHP、JavaScript，目前深耕于前端
+                （虽然刚刚起步）。这是我学习前端的第一年，从今年3月到现在转眼已经9个月了，这一路磕磕绊绊，质疑过
+                自己，曾经对自己的薄弱的技术水平产生过深深的无耐，但是我一直都坚信着只要我坚持下去，将来的我可以
+                用技术改变我身边的事物、朋友。并且，接下来还有很多个一年，就如我现在的座右铭一般“坚持，时间会给你
+                答案”，所以，如果此时看到这的你也是一个初入，又或者是一个前端技术小白，希望你和我一样能一直
+                坚持走在这条路上，莫问收获，但问耕耘，前端的路上我与你同在。
+            </div>
+        </div>
+        <div class="concat">
+           联系我 
+        </div>
     </div>
 </template>
 
@@ -8,16 +26,49 @@
 export default {
     title: "关于我",
     metaInfo: {
-        meta: [
-        //     {
-        //         name: "keywords",
-        //         content: "Vue, Vue SSR, Node, About"
-        //     }
-        ]
+        meta: []
+    },
+    data() {
+        return {
+            linkArr: [
+					{icon: 'icon-csdn', name: 'CSDN', address: 'https://blog.csdn.net/qq_42049445'},
+					{icon: 'icon-GitHub', name: 'GitHub', address: 'https://github.com/WJCHumble'},
+					{icon: 'icon-email', name: 'QQ邮箱', content: '1957503379@qq.com'},
+					{icon: 'icon-weixin', name: '微信公众号', content: '微信搜索公众号Code center || 扫描下方二维码'},
+				]
+        }
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.about 
+    margin-top 20px
+    padding 20px
+    min-height 630px
+    background #fff
+    word-spacing 4px
+.title, .concat
+    position relative
+    width 100%
+    font-size 20px
+    margin-bottom 20px
+    &:after
+        position absolute
+        top 18px
+        left 94px
+        content ''
+        width 86%
+        height 1px
+        background #494949
+.concat
+    margin-top 20px
+    &:after
+        width 90%
+        left 70px
+.avatar
+    float left
+    margin-right 14px
+    img
+        width 120px
 </style>
--- src/view/Blog/index.vue
+++ src/view/Blog/index.vue
@@ -4,8 +4,12 @@
         <div class="category">
             <div class="category-item" v-for="(category, index) in categoryList" :key="index">
                 <span class="item-name">{{category.category}}</span>
-                <span v-for="(item, index) in category.list" :key="index">{{item}}</span>
+                <span v-for="(item, index) in category.list" :key="index" @click="selected = item" :class="{selected: item === selected}">{{item}}</span>
             </div>
+        </div>
+        <!-- 对应分类的内容 -->
+        <div class="content">
+
         </div>
     </div>
 </template>
@@ -55,7 +59,8 @@ export default {
                         'Redis'
                     ]
                 },
-            ]
+            ],
+            selected: 'HTML' // 记录当前选中项
         }
     }
 }
@@ -63,15 +68,21 @@ export default {
 
 <style lang="stylus" scoped>
 .category
+    padding 20px
+    border-radius 4px
+    background #fff
     div
         margin-bottom 10px
     .category-item
         span
-            margin-right 60px
+            margin-right 40px
+            padding 4px 8px
+            border-radius 4px
             font-size 12px
             &:hover
                 cursor pointer
                 color  #f60
+                background #F9FAFB
         span:nth-child(2)
             margin-left 40px
         .item-name
@@ -81,4 +92,13 @@ export default {
             font-weight bold
             &:hover
                 cursor text
+                color #34495e
+.content
+    margin-top 10px
+    min-height 400px
+    padding 20px
+    background #fff
+.selected
+    background #f60
+    color #fff
 </style>
